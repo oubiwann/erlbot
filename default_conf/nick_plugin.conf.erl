966ecd9c596e07eff51b080684ca6353952d0d33
Duncan McGreggor <duncan@cogitat.io>
2014-03-23 13:38:27 -0500

    Removed trailing spaces.

--- default_conf/nick_plugin.conf.erl
+++ default_conf/nick_plugin.conf.erl
@@ -1,5 +1,5 @@
 %%% Default settings always get loaded
-%%% Copy tuple's you wish to change to user_conf/user.conf.erl 
+%%% Copy tuple's you wish to change to user_conf/user.conf.erl
 
 {nick_plugin, [
     % nick - nickname to set in IRC
